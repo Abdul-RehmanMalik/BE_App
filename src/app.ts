3a8dc48145fa28a1e803049d3a67c1a5716cbb6d
Abdul Rehman Malik <abdul.rehman.malik@tetrahex.com>
2023-05-10 16:43:35 +0500

    feat: api code refactor

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose, { ConnectOptions } from "mongoose";
-import userRoutes from "./routes/userRoutes";
-import authRoutes from "./routes/authRoutes";
+import { connectToMongoDB } from "./util/dbConnection";
+import router from "./routes/index";
 const app = express();
 dotenv.config();
 // port from env file
 const port = process.env.PORT;
-// connect to MongoDB
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
-    console.log(" Successfully Connected to MongoDB!");
-  } catch (error) {
-    console.error("Error!connecting to MongoDB:", error);
-  }
-})();
+
+// Connect to mongoDb
+connectToMongoDB();
 app.use(express.json());
 
-// User router
-app.use("/users", userRoutes);
-
-// Auth router
-app.use("/auth", authRoutes);
+// router
+app.use("/router", router);
 
 //app listening on Port
 app.listen(port, () => {
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -33,3 +33,26 @@ export const login = async (req: Request, res: Response) => {
     res.status(500).json({ error: err.message });
   }
 };
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password, name, address } = req.body;
+    // pasword hashing
+    const invalidEmail = await User.findOne({ email });
+    // to check whether user already exists or not
+    if (invalidEmail) {
+      res.status(400).json(invalidEmail);
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      name,
+      address,
+    });
+    res.status(201).json(user);
+  } catch (err: any) {
+    console.log("Failed to reg new user");
+    res.status(500).json({ error: err.message });
+  }
+};
